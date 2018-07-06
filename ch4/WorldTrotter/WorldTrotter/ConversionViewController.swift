cacce12f18dbdd855eb0491073ed3b9b5413a880
USER <tlsqlfhdnsdl@naver.com>
2018-07-06 11:48:44 +0900

    ch4

---
+++ ch4/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by 김영호 on 05/07/2018.
+//  Copyright © 2018 Yeongho. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField:UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsius()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    @IBAction func fahrenheitFieldEditiong(_ textField: UITextField) {
+//                    celsiusLabel.text = textField.text
+
+//        if let text = textField.text, !text.isEmpty {
+//            celsiusLabel.text = textField.text
+//        }
+//        else {
+//            celsiusLabel.text = "???"
+//        }
+
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func updateCelsius() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        updateCelsius()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
