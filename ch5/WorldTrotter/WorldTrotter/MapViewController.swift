11ea84ea79ad90df83afa3bb58abe49accbc14d7
USER <tlsqlfhdnsdl@naver.com>
2018-07-06 11:45:46 +0900

    ch5

---
+++ ch5/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by USER on 2018. 7. 6..
+//  Copyright © 2018년 Yeongho. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("MapViewController loaded its view")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ch5/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by 김영호 on 04/07/2018.
+//  Copyright © 2018 Yeongho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//
+//        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+//        firstView.addSubview(secondView)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
